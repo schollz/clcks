5febea13e6b0980bb81d974a1eca05640373fee5
Zack Scholl <zack.scholl@gmail.com>
2020-08-26 07:54:15 +0700

    getting there

--- clcks.lua
+++ clcks.lua
@@ -32,6 +32,15 @@ state={
   update_screen=false,
 }
 
+params={
+  tempo=90,
+  level=1,
+  rate=1,
+  repeats=3,
+  subdivided=1,
+  randomizer="off",
+}
+
 function init()
   audio.level_adc_cut(1) -- send audio input to softcut input
   audio.level_monitor(1)
@@ -58,20 +67,10 @@ function init()
   
   -- initialize timer
   timer=metro.init()
-  timer.time=60/params:get("clock_tempo")/16
+  timer.time=60/params.tempo/16
   timer.count=-1
   timer.event=update_timer
   timer:start()
-  
-  -- set parameters
-  params:set_action("clock_tempo",update_clock)
-  params:add_control("beats","beats",controlspec.new(1,16,"lin",1,1))
-  params:add_control("repeat","repeat",controlspec.new(1,10,"lin",1,3))
-  params:add_control("rate","rate",controlspec.new(-4,4,"lin",1,0.1))
-  params:set_action("rate",update_rate)
-  params:add_control("level","level",controlspec.new(0,1,"lin",1,0.1))
-  params:set_action("level",update_level)
-  params:add_control("randomizer","randomizer",{"on","off"})
   
   -- position poll
   -- monitor one position to get both, since they are synced
@@ -87,7 +86,7 @@ end
 function update_level(x)
   if state.activated then
     for i=1,2 do
-      softcut.level(i,x)
+      softcut.level(i,params.level)
     end
   end
 end
@@ -95,7 +94,7 @@ end
 function update_rate(x)
   if state.activated then
     for i=1,2 do
-      softcut.rate(i,x)
+      softcut.rate(i,params.rate)
     end
   end
 end
@@ -107,7 +106,7 @@ end
 function update_timer()
   if state.activated then
     state.time=state.time+const_time_per_refresh
-    state.tick=round(state.time/(60/params:get("clock_tempo")))%2==1
+    state.tick=round(state.time/(60/params.tempo))%2==1
     
     -- get repeat number
     current_repeat=state.repeats
@@ -118,12 +117,12 @@ function update_timer()
     
     -- get beat number
     current_beat_number=state.beat
-    state.beat=1+math.floor(state.time/(loop_length()))%params:get("beats")
+    state.beat=1+math.floor(state.time/(loop_length()))%params.subdivided
     if state.beat~=current_beat_number then
       state.update_screen=true
     end
     
-    if params:get("repeat")<10 and state.time>=loop_length()*params:get("repeat") then
+    if params.repeats<10 and state.time>=loop_length()*params.repeats then
       deactivate_basic()
     end
   end
@@ -133,7 +132,7 @@ function update_timer()
 end
 
 function loop_length()
-  return 60/params:get("clock_tempo")/params:get("beats")
+  return 60/params.tempo/params.subdivided
 end
 
 function activate_basic(monitor_mode)
@@ -146,7 +145,7 @@ function activate_basic(monitor_mode)
   if prev_position<1 then
     prev_position=1
   end
-  slew_rate=params:get("repeat")*loop_length()*4
+  slew_rate=params.repeats*loop_length()*4
   if slew_rate<0 then
     slew_rate=loop_length()
   end
@@ -159,8 +158,8 @@ function activate_basic(monitor_mode)
     softcut.loop_end(i,current_position)
     softcut.rate_slew_time(1,2*slew_rate)
     softcut.level_slew_time(i,slew_rate)
-    softcut.rate(i,params:get("rate"))
-    softcut.level(i,params:get("level"))
+    softcut.rate(i,params.rate)
+    softcut.level(i,params.level)
   end
   state.time=0
   state.repeats=0
@@ -187,13 +186,13 @@ end
 
 function randomizer()
   clock.sleep(math.random(0,10))
-  if params:get("randomizer")=="off" then
+  if params.randomizer=="off" then
     do return end
   end
-  params:set("level",math.random(0,1))
-  params:set("rate",math.random(-4,4))
-  params:set("repeat",round(math.random(1,5)))
-  params:set("beats",round(math.random(1,8)))
+  params.level=math.random(0,1)
+  params.rate=math.random(-4,4)
+  params.repeats=round(math.random(1,5))
+  params.subdivided=round(math.random(1,8))
   monitor_mode=round(math.random())
   state.update_screen=true
   activate_basic(monitor_mode)
@@ -204,21 +203,23 @@ function enc(n,d)
   if n==1 then
     if state.shift then
     else
-      params:set("repeat",util.clamp(params:get("repeat")+d,1,10))
+      params.repeats=util.clamp(params.repeats+d,1,10)
     end
   elseif n==2 then
     if state.shift then
-      params:set("clock_tempo",util.clamp(params:get("clock_tempo")+d,10,500))
+      params.tempo=util.clamp(params.tempo+d,10,500)
     else
-      params:set("level",util.clamp(params:get("level")+d/100,0,1))
+      params.level=util.clamp(params.level+d/100,0,1)
+      update_level(0)
     end
   elseif n==3 then
     if state.shift then
-      params:set("beats",util.clamp(params:get("beats")+d,1,16))
+      params.subdivided=util.clamp(params.subdivided+d,1,16)
     else
       -- turning ccw sets to reverse
       -- turning cw sets to forward
-      params:set("rate",sign(d)*math.abs(util.clamp(params:get("rate")+d/100,-4,4)))
+      params.rate=sign(d)*math.abs(util.clamp(params.rate+d/100,-4,4))
+      update_rate(0)
     end
   end
   state.update_screen=true
@@ -228,9 +229,9 @@ function key(n,z)
   if n==2 and z==1 then
     if state.shift then
       -- reset final parameters
-      params:set("randomizer","off")
-      params:set("rate",1)
-      params:set("level",1)
+      params.randomizer="off"
+      params.rate=1
+      params.level=1
     else
       -- initiate with monitor mode
       activate_basic(1)
@@ -238,7 +239,7 @@ function key(n,z)
   elseif n==3 and z==1 then
     if state.shift then
       -- randomize chop
-      params:set("randomizer","on")
+      params.randomizer="on"
       clock.run(randomizer)
     else
       -- initate without monitor mode
@@ -263,14 +264,14 @@ function redraw()
   
   -- draw bpm
   screen.move(3+shift_amount,8+shift_amount)
-  screen.text(params:get("clock_tempo"))
+  screen.text(params.tempo)
   metro_icon(16+shift_amount,3+shift_amount)
   
   -- draw beat subidivision boxes
   x=34+shift_amount
   y=4+shift_amount
   w=3
-  show_beats=params:get("beats")
+  show_beats=params.subdivided
   if state.activated then
     show_beats=current_beat_number
   end
@@ -285,18 +286,19 @@ function redraw()
   x=2 -- x, y are the top left hand point
   y=20
   h=40
-  w=round(120/params:get("repeat"))
-  show_repeats=params:get("repeat")
+  w=round(120/params.repeats)
+  show_repeats=params.repeats
   if state.activated then
     show_repeats=state.repeats
   end
-  short_hand_angle=180*math.abs(params:get("rate"))/4+90
+  short_hand_angle=180*math.abs(params.rate)/4+90
   rinitial=w/2
-  rfinal=w/2*params:get("level")
+  rfinal=w/2*params.level
+  print(params.level)
   for i=1,show_repeats do
     -- default: interpolate level between beginning and end
-    r=rinitial*(params:get("repeat")-i)/(params:get("repeat")-1)
-    r=r+rfinal*(1-(params:get("repeat")-i)/(params:get("repeat")-1))
+    r=rinitial*(params.repeats-i)/(params.repeats-1)
+    r=r+rfinal*(1-(params.repeats-i)/(params.repeats-1))
     if i==1 then
       r=rinitial
     elseif i==state.repeats then
@@ -307,21 +309,28 @@ function redraw()
     -- draw ellipse with short radius showing interpolated level
     center={x+w/2,y+w/2}
     screen.move(center[1],y)
-    screen.curve(center[1],y+h,center[1]-r,y,center[1]-r,y+h)
+    screen.curve(center[1]-r,y,center[1]-r,y+h,center[1],y+h)
     screen.stroke()
     screen.move(center[1],y)
-    screen.curve(center[1],y+h,center[1]+r,y,center[1]+r,y+h)
+    screen.curve(center[1]+r,y,center[1]+r,y+h,center[1],y+h)
     screen.stroke()
     
     -- short hand indicates absolute rate
-    screen.arc(center[1],center[2],r/2,short_hand_angle,short_hand_angle)
+    screen.move(center[1],center[2])
+    screen.line(center[1]+r/3*math.sin(math.rad(short_hand_angle)),center[2]+r/3*math.cos(math.rad(short_hand_angle)))
+    screen.stroke()
     
     -- long hand indicates direction of rate
     angle=(i-1)*36
-    if params:get("rate")<0 then
+    if params.rate>0 then
       angle=360-angle
     end
-    screen.arc(center[1],center[2],r,angle,angle)
+    angle=angle-180
+    angle=-1*angle
+    
+    screen.move(center[1],center[2])
+    screen.line(center[1]+r*math.sin(math.rad(angle)),center[2]+r*math.cos(math.rad(angle)))
+    screen.stroke()
     
     -- update x position
     x=x+w -- TODO: try using r instead
